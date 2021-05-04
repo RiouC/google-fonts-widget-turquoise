604f3c4126427333579be15e6607db983058018b
RiouC <riou.christophe@protonmail.com>
2021-05-04 21:48:27 +0200

    [WIP][Handlers] add onChange handlers to remove warnings temporarily

--- src/components/WidgetSelect.js
+++ src/components/WidgetSelect.js
@@ -1,8 +1,12 @@
 const WidgetSelect = () => {
+    const handleChangeSelect = (e) => {
+        console.log(e);
+        return e;
+    }
     return (
         <>
             <label className="fw-bold mb-2" htmlFor="sort">Afficher des polices</label>
-            <select id="sort" className="form-select mb-4">
+            <select id="sort" className="form-select mb-4" onChange={handleChangeSelect}>
                 <option value="date">Les plus récentes</option>
                 <option value="popularity">Les plus populaires</option>
                 <option value="trending">Top 10 trending</option>
--- src/components/WidgetSize.js
+++ src/components/WidgetSize.js
@@ -1,8 +1,12 @@
 const WidgetSize = () => {
+    const handleChangeInput = (e) => {
+        console.log(e);
+        return e;
+    }
     return (
         <>
             <label htmlFor="range" className="form-label fw-bold mb-3">La taille de police</label>
-            <input type="range" className="form-range" id="range" min="8" max="48" step="1" value="20" />
+            <input type="range" className="form-range" id="range" min="8" max="48" step="1" value="20" onChange={handleChangeInput} />
         </>
     )
 }
