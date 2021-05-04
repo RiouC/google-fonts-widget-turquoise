25ea71e087e25842872b8793df6fa0ad81785176
RiouC <riou.christophe@protonmail.com>
2021-05-04 21:46:40 +0200

    [Font,FontsList] map correctly

--- src/components/Font.js
+++ src/components/Font.js
@@ -1,27 +1,35 @@
 import GoogleFontLoader from 'react-google-font-loader';
 
 const Font = (props) => {
-    //const { family } = props;
+    const { family, variants, category, files } = props;
     return (
         <>
-            <GoogleFontLoader
-                fonts={[
-                    {
-                        font: 'Roboto',
-                        weights: [400, '400i'],
-                    },
-                    {
-                        font: 'Roboto Mono',
-                        weights: [400, 700],
-                    },
-                ]}
-                subsets={['cyrillic-ext', 'greek']}
-            />
+          <GoogleFontLoader
+            fonts={[
+                {
+                    font: family,
+                    weights: [400, '400i'],
+                },
+                {
+                    font: 'Roboto Mono',
+                    weights: [400, 700],
+                },
+            ]}
+            subsets={['cyrillic-ext', 'greek']}
+          />
 
-            <p style={{ fontFamily: 'Roboto Mono, monospaced' }}>This will be in Roboto Mono!</p>
-            <p style={{ fontFamily: 'Roboto, sans-serif' }}>This will be in Roboto!</p>
+          <article className="col-lg-6 mb-4">
+            <div className="shadow-sm border p-3 h-100">
+              <h2 className="h6 d-flex aling-items-center justify-content-between">
+                <span style={{ fontFamily: `${family}, ${category}` }}>{family}</span><small>{variants.length} variant(s)</small></h2>
+              <p><span className="badge bg-dark">DISPLAY</span>
+              </p>
+              <p style={{ fontFamily: family, fontSize: "20px" }} className="sample">Portez ce vieux whisky au juge blond qui fume !? 0123456789</p>
+              <a rel="noopener noreferrer" target="_blank" className="text-danger" href={`${files[0]}`}>Voir sur Google Fonts (ouvre un nouveau tab)</a>
+            </div>
+          </article>
         </>
-    )
-}
+    );
+};
 
 export default Font;
--- src/components/FontsList.js
+++ src/components/FontsList.js
@@ -30,23 +30,15 @@ const FontsList = () => {
 
     return (
         <div className="col-lg-9">
-            <section className="row mb-5">
-                <h2 className="mb-3"><span className="badge bg-danger">Les plus récentes</span></h2>
+          <section className="row mb-5">
+            <h2 className="mb-3"><span className="badge bg-danger">Les plus récentes</span></h2>
 
-                {fontsList.map(ft => <Font key={ft.item} kind={ft.kind} family={ft.family} variant={ft.variant} />)}
+            {fontsList.map(ft => <Font key={ft.family}
+                                              family={ft.family}
+                                              variants={ft.variants}
+                                              files={ft.files}/>)}
 
-                <article className="col-lg-6 mb-4">
-                    <div className="shadow-sm border p-3 h-100">
-                        <h2 className="h6 d-flex aling-items-center justify-content-between">
-                            <span>Orelega One</span><small>1 variant(s)</small></h2>
-                        <p><span className="badge bg-dark">DISPLAY</span>
-                        </p>
-                        <p style={{ fontFamily: "Orelega One", fontSize: "20px" }} className="sample">Portez ce vieux whisky au juge blond qui fume !? 0123456789</p>
-                        <a rel="noopener noreferrer" target="_blank" className="text-danger" href="https://fonts.google.com/specimen/Orelega+One">Voir sur Google Fonts (ouvre un nouveau tab)</a>
-                    </div>
-                </article>
-
-            </section>
+          </section>
         </div>
     );
 };
