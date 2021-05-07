6d201acdd8425a4af9f4025ce13b445a3537760d
RiouC <riou.christophe@protonmail.com>
2021-05-07 16:13:49 +0200

    [Font] improve code readability

--- src/components/Font.js
+++ src/components/Font.js
@@ -21,11 +21,24 @@ const Font = (props) => {
           <article className="col-lg-6 mb-4">
             <div className="shadow-sm border p-3 h-100">
               <h2 className="h6 d-flex aling-items-center justify-content-between">
-                <span style={{ fontFamily: `${family}, ${category}` }}>{family}</span><small>{variants.length} variant(s)</small></h2>
+                <span style={{ fontFamily: `${family}, ${category}` }}>
+                  {family}
+                </span>
+                <small>
+                   {variants.length} variant(s)
+                </small>
+              </h2>
               <p><span className="badge bg-dark">DISPLAY</span>
               </p>
-              <p style={{ fontFamily: family, fontSize: "20px" }} className="sample">Portez ce vieux whisky au juge blond qui fume !? 0123456789</p>
-              <a rel="noopener noreferrer" target="_blank" className="text-danger" href={`${files[0]}`}>Voir sur Google Fonts (ouvre un nouveau tab)</a>
+              <p style={{ fontFamily: family, fontSize: "20px" }}
+                 className="sample">
+                Portez ce vieux whisky au juge blond qui fume !? 0123456789
+              </p>
+              <a rel="noopener noreferrer" target="_blank"
+                 className="text-danger"
+                 href={`${files[0]}`}>
+                Voir sur Google Fonts (ouvre un nouveau tab)
+              </a>
             </div>
           </article>
         </>
@@ -33,3 +46,5 @@ const Font = (props) => {
 };
 
 export default Font;
+
+// 28 : {variants.length} variant(s)
