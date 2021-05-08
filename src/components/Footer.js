5e61785bdb7e36af0a69e20d3701f746231f5814
RiouC <riou.christophe@protonmail.com>
2021-05-08 14:23:09 +0200

    [Footer] improve footer with symbols and colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
         <footer className="p-3 text-center bg-dark text-white">
-          Team Turquoise &amp; for Alyra
+          Team Turquoise with &#9775; ,
+          <span style={{color: "green"}}>&#9752;</span>
+          &#8523; <span style={{color: "cyan"}}>&#9883;</span> for Alyra
         </footer>
     );
 };
