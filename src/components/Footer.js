1c7ea6194386b38503a9e4f7f9024d378a38671b
RiouC <riou.christophe@protonmail.com>
2021-05-10 14:16:35 +0200

    [Footer] change color for clover contrast

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = ({ darkMode, setDarkMode }) => {
                 ? "p-3 text-center bg-dark text-white border-top border-white"
                 : "p-3 text-center bg-dark text-white"}>
           Team Turquoise with &#9775; ,
-          <span style={{color: "green"}}>&#9752;</span>
+          <span style={{color: "#27EC27"}}>&#9752;</span>
           &#8523; <span style={{color: "cyan"}}>&#9883;</span> for Alyra
         </footer>
     );
