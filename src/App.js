d92e366fdebe161ee0e160a6e63f3b70f4923c39
RiouC <riou.christophe@protonmail.com>
2021-05-10 03:02:34 +0200

    add darkmode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FontsApp from './components/FontsApp';
 
 function App() {
+    const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) || false);
+
+  useEffect(() => {
+      document.body.className = darkMode ? 'bg-dark text-light' : 'bg-white';
+      localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+
+
   return (
     <div className="App">
 
-      <Header />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
 
       <FontsApp />
 
       <Footer />
+
     </div >
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
-const Header = () => {
+const Header = ({ darkMode, setDarkMode }) => {
+    const handleChangeDarkMode = () => {
+        setDarkMode(!darkMode)
+    };
     return (
         <header className="p-5 bg-danger text-white text-center">
-            <h1 className="display-1" id="header-title">Google Fonts™&nbsp;? Widget</h1>
-            <p className="h4">Les plus récentes, <i>trendy</i> et populaires polices Google à portée de main.</p>
+          <h1 className="display-1" id="header-title">Google Fonts™&nbsp;? Widget</h1>
+          <p className="h4">Les plus récentes, <i>trendy</i> et populaires polices Google à portée de main.</p>
+          <div className="form-check form-switch align-top d-inline-block">
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              {darkMode ? "☽" : "☀"}
+            </label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              aria-label="Dark Mode"
+              key="flexSwitchCheckDefault"
+              name="flexSwitchCheckDefault"
+              onChange={handleChangeDarkMode}
+              checked={darkMode}>
+            </input>
+          </div >
         </header>
     );
 };
